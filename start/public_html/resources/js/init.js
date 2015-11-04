9acbc7339f15a809dff872892d8798ddc9810134
Bakum Viacheslav <bakum.v@gmail.com>
2015-11-04 16:12:16 +0200

    first

---
+++ start/public_html/resources/js/init.js
@@ -0,0 +1,13 @@
+$(function(){
+	
+    $('.slider3').mobilyslider({
+        transition: 'fade',
+        animationSpeed: 800,
+        bullets: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrowsHide: false
+    });
+	
+	
+});
